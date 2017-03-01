b33ec0b519a6843ef190c1fd92372ec643852ebf
abr2001 <f1@oren.ru>
2017-03-01 23:16:12 +0500

    test belongs to user

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
 
   login_user
-  let(:question) { create(:question, user: @user) }
+  let(:question) { create(:question) }
 
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'saves the new answer in the database' do
         expect { post :create, params: { answer: attributes_for(:answer), question_id: question } }.to change(question.answers, :count).by(1)
+      end
+      it 'the answer belongs to user' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        expect(Answer.last.user).to eq(@user)
       end
     end
 
@@ -29,6 +33,13 @@ RSpec.describe AnswersController, type: :controller do
       it 'redirect to question show' do
         delete :destroy, params: { id: answer, question_id: question }
         expect(response).to redirect_to question_path
+      end
+    end
+
+    context 'not author delete answer' do
+      let!(:answer2) { create(:answer, :with_user, question: question) }
+      it 'can not delete answer in database' do
+        expect { delete :destroy, params: { id: answer2, question_id: question } }.to_not change(Answer, :count)
       end
     end
   end
