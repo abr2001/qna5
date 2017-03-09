93f6fb2e01f4b631c99a442e8e7d4a86016fd57f
abr2001 <f1@oren.ru>
2017-03-09 20:32:18 +0500

    edit_answer_spec

---
+++ spec/features/answers/edit_answer_spec.rb
@@ -0,0 +1,31 @@
+require 'features/features_helper'
+
+feature 'edit answer for question', %q{
+  In order to edit answers for question
+  Authenticated user
+  I want change answer for question
+} do
+
+  let!(:question) { create(:question) }
+  let!(:user) { create(:user) }
+  let!(:answer) { create(:answer, user: user, body: 'my answer for question', question: question) }
+
+  scenario 'The user edit their answer', js: true do
+    login_user
+    visit question_path(question.id)
+    within('.answer') do
+      click_on 'Edit'
+      fill_in 'Answer', with: 'my new answer'
+      click_on 'Update'
+    end
+    expect(page).to have_content 'my new answer'
+  end
+
+  scenario 'a non-authenticated user edit own answer' do
+  end
+
+  let!(:another_user) { create(:user) }
+  scenario "The user tries to edit someone else's answer" do
+  end
+
+end
