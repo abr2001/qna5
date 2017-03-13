1bcae7e106a58f0ce3b76eafbfb3e1ac4ee6f40a
abr2001 <f1@oren.ru>
2017-03-13 11:37:52 +0500

    add best to answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
   validates :body, :user, presence: true
+  validate :best_only_one
+
+  private
+
+  def best_only_one
+    if Answer.where(best: true).where(question_id: self.question_id).count > 0
+      errors.add(:base, 'Must be the best only one answer')
+    end
+  end
 end
