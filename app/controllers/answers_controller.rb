db4de28cab0f8c84b4673a51134370f7e7960bf6
abr2001 <f1@oren.ru>
2017-03-14 21:57:30 +0500

    set best

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,9 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   before_action :load_answer, only: [:destroy, :update, :set_best]
+  before_action :load_question, only: [:update, :set_best, :create]
 
   def create
-    @question = Question.find(params[:question_id])
     @answer = @question.answers.create(answer_params.merge(user: current_user))
   end
 
@@ -19,12 +19,12 @@ class AnswersController < ApplicationController
   def update
     if current_user.author_of?(@answer)
       @answer.update(answer_params)
-      @question = @answer.question
     end
   end
 
   def set_best
     if current_user.author_of?(@answer.question)
+      Answer.where(question_id: @answer.question).where(best: true).update_all(best: false)
       @answer.update_attributes(best: true)
     end
   end
@@ -37,6 +37,10 @@ class AnswersController < ApplicationController
 
   def load_answer
     @answer = Answer.find(params[:id] || params[:answer_id])
+  end
+
+  def load_question
+    @question = Question.find(params[:question_id])
   end
 
 
