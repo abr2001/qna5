54d6e1935ae9ba63887eb078426b059114cc93cf
abr2001 <f1@oren.ru>
2017-03-20 09:55:26 +0500

    Question to accept nested attributes for attachments

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,5 @@ class Question < ApplicationRecord
   has_many :attachments, as: :attachable, dependent: :destroy
   belongs_to :user
   validates :title, :body, :user, presence: true
+  accepts_nested_attributes_for :attachments
 end
