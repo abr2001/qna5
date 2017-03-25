d211ed7a6c237547fe489cd2fea273089fd40667
abr2001 <abr2001@mail.ru>
2017-03-26 00:06:40 +0500

    add test controller attachments

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,6 +1,6 @@
 class AttachmentsController < ApplicationController
   def destroy
     @attachment = Attachment.find(params[:id])
-    @attachment.destroy
+    @attachment.destroy if current_user.author_of?(@attachment.attachable)
   end
 end
--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe AttachmentsController, type: :controller do
 
+  login_user
+  let(:question) { create(:question, user: @user) }
+  let!(:attachment) { create(:attachment, attachable: question)}
+
+  describe 'DELETE #destroy' do
+    before { attachment }
+    context 'autor delete attachment' do
+      it 'delete attachment in the database' do
+        expect { delete :destroy, format: :js, params: { id: attachment } }.to change(Attachment, :count).by(-1)
+      end
+    end
+
+    context 'not autor delete attachment' do
+      let!(:question2) { create(:question) }
+      let!(:attachment2) { create(:attachment, attachable: question2)}
+      before { attachment2 }
+      it 'delete question in the database' do
+        expect { delete :destroy, format: :js, params: { id: attachment2 } }.to_not change(Attachment, :count)
+      end
+    end
+  end
+
 end
