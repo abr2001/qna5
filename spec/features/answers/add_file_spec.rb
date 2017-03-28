6d06f916dd233e3696f57ea82ca8c4feaa2bca65
abr2001 <abr2001@mail.ru>
2017-03-28 15:03:57 +0600

    test add many files

--- spec/features/answers/add_file_spec.rb
+++ spec/features/answers/add_file_spec.rb
@@ -18,8 +18,11 @@ feature 'Add file to answers', %q{
     fill_in 'Answer', with: 'text text text'
     click_on 'Add attachment'
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Add attachment'
+    all('.fields').last.attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
     click_on 'Create'
 
-    expect(page).to have_link 'spec_helper.rb', href: question.answers.last.attachments.last.file.url
+    expect(page).to have_link 'spec_helper.rb', href: question.answers.last.attachments.first.file.url
+    expect(page).to have_link 'rails_helper.rb', href: question.answers.last.attachments.last.file.url
   end
 end
