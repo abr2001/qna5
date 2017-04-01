e993d6bd035dfea3410da15e47dd0b15b413aff4
abr2001 <abr2001@mail.ru>
2017-04-02 00:13:55 +0600

    fix destroy attachment

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,6 +1,10 @@
 class AttachmentsController < ApplicationController
   def destroy
     @attachment = Attachment.find(params[:id])
-    @attachment.destroy if current_user.author_of?(@attachment.attachable)
+    if current_user.author_of?(@attachment.attachable)
+      @attachment.destroy
+    else
+      head :forbidden
+    end
   end
 end
--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -21,6 +21,10 @@ RSpec.describe AttachmentsController, type: :controller do
       it 'delete question in the database' do
         expect { delete :destroy, format: :js, params: { id: attachment2 } }.to_not change(Attachment, :count)
       end
+      it 'response with status forbidden' do
+        delete :destroy, format: :js, params: { id: attachment2 }
+        expect(response).to have_http_status(:forbidden)
+      end
     end
   end
 
