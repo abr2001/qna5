58f50effa6dad2ecf4641a37172958a61a07a15b
abr2001 <abr2001@mail.ru>
2017-04-07 13:43:52 +0600

    test model rate

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -2,5 +2,17 @@ class Rate < ApplicationRecord
   belongs_to :user
   belongs_to :ratable, polymorphic: true, optional: true
 
-  validates :user, presence: true
+  validates :user, :value, presence: true
+  validate :check_user_already_has_rate
+
+  private
+
+  def check_user_already_has_rate
+    if Rate.where(user_id: user_id).
+      where(ratable_id: ratable_id).
+      where(ratable_type: ratable_type).exists?
+      errors.add(:base, 'User already has rate')
+    end
+  end
+
 end
