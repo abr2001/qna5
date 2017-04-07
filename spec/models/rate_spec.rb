fe5072106242597be6536b266206a33c2c22f357
abr2001 <abr2001@mail.ru>
2017-04-07 13:43:52 +0600

    test model rate

--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Rate, type: :model do
   it { should belong_to(:ratable) }
 
   it { should validate_presence_of :user }
+  it { should validate_presence_of :value }
+
+
+  let!(:user) { create :user }
+  let!(:question) { create :question }
+  describe 'User can not re-rate' do
+    before { rate = question.rates.create(user_id: user.id) }
+    it 'get valid errors' do
+      expect(rate.errors.messages.to_s).to include("User already_has_rate")
+    end
+  end
+
 end
