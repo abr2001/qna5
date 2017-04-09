2dcddf082550898aba64353f1ea82097078e58dc
abr2001 <abr2001@mail.ru>
2017-04-09 12:55:02 +0600

    fixes 2

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -3,16 +3,12 @@ class Rate < ApplicationRecord
   belongs_to :ratable, polymorphic: true, optional: true
 
   validates :value, presence: true
-  before_validation :check_user_already_has_rate
+  validate :check_user_already_has_rate
 
   private
 
   def check_user_already_has_rate
-    if Rate.where(user_id: user_id).
-        where(ratable_id: ratable_id).
-        where(ratable_type: ratable_type).exists?
-      errors.add(:base, 'You already has rate')
-    end
+    errors.add(:base, 'You already has rate') if Rate.where(user_id: user_id, ratable: ratable).exists?
   end
 
 end
--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Rate, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:ratable) }
 
-  it { should validate_presence_of :user }
+
   it { should validate_presence_of :value }
 
 
