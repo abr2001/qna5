ac2597a9dafdf016cd88d8c6df0ad58c2f4c3fe0
abr2001 <abr2001@mail.ru>
2017-11-17 15:51:43 +0500

    create user.find_for_oauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -11,5 +11,21 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   private
   def sign_in_provider
+    if auth.empty?
+      redirect_to questions_path
+    else
+      @user = User.find_for_oauth(auth)
+      if @user && @user.persisted?
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message(:notice, :success, kind: auth.provider) if is_navigational_format?
+      else
+        render 'omniauth_callbacks/enter_email', locals: { auth: auth}
+      end
+    end
   end
+
+  def auth
+    request.env['omniauth.auth'] || OmniAuth::AuthHash.new(params['auth'])
+  end
+
 end
