f4b2aeb51fdf51cdf539a9fa0a67ce09df1b6990
abr2001 <abr2001@mail.ru>
2017-11-30 11:37:28 +0500

    Create shared/api_authorization.rb

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -2,17 +2,7 @@ require 'rails_helper'
 
 describe 'Profile API' do
   describe 'GET /me' do
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get '/api/v1/profiles/me', params: {format: :json}
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/profiles/me', params: {format: :json, access_token: '1234'}
-        expect(response.status).to eq 401
-      end
-    end
+    it_behaves_like "API Authenticable"
 
     context 'authorized' do
       let(:me) { create(:user) }
@@ -30,21 +20,14 @@ describe 'Profile API' do
         it { expect(response.body).to_not have_json_path(attr) }
       end
     end
+    def do_request(options = {})
+      get '/api/v1/profiles/me', params: {format: :json}.merge(options)
+    end
   end
 
 
   describe 'GET /index' do
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get '/api/v1/profiles/me', params: {format: :json}
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/profiles/me', params: {format: :json, access_token: '1234'}
-        expect(response.status).to eq 401
-      end
-    end
+    it_behaves_like "API Authenticable"
 
     context 'authorized' do
       let(:me) { create(:user) }
@@ -66,6 +49,9 @@ describe 'Profile API' do
       %w(password encrypted_password).each do |attr|
         it { expect(response.body).to_not have_json_path(attr) }
       end
+    end
+    def do_request(options = {})
+      get '/api/v1/profiles/', params: {format: :json}.merge(options)
     end
   end
 end
